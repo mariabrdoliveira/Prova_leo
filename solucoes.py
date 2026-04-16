89c3b610ee9185fe7d96636645023f3b13de87be
mariabrdoliveira <mariabrdoliveira@outlook.com>
2026-04-16 10:10:14 -0300

    criacao do arquivo inicial

---
+++ solucoes.py
@@ -0,0 +1,14 @@
+def sao_anagramas(string1, string2):
+    # TODO: Implemente a logica
+    pass
+    
+
+def cifra_de_cesar(texto, deslocamento):
+    # TODO: Implemente a logica
+    pass
+    
+
+def valida_cpf(cpf_string):
+    # TODO: Implemente a logica
+    pass
+    
